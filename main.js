ac895b7d97496107591a4230670575ad17164565
Sarah Beth Langley <sarahbethdoescode@gmail.com>
2022-10-07 10:17:12 -0500

    this is complete-ly full of functions

---
+++ main.js
@@ -0,0 +1,13 @@
+const quarterValue = (notScary) => {
+    const quarter = notScary * .25
+
+    return quarter 
+
+}
+
+// Invoke the function and store the return value
+const returnedNumber = quarterValue(100)
+
+
+// Log the return value
+console.log(returnedNumber)
